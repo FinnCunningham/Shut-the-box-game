69ecda2e998b61c2720556fd1ed84e2bc13a2040
Finn <s5117817@bournemouth.ac.uk>
2020-02-11 22:04:52 +0000

    Added functionalty of finding all possibilities that the player can make. Started to add highlights to the possibilities.

--- app/src/main/java/com/example/finn/kotlinapp1/MainActivity.kt
+++ app/src/main/java/com/example/finn/kotlinapp1/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.finn.kotlinapp1
 
 import android.content.pm.ActivityInfo
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,13 +16,10 @@ import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-import android.support.v4.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.view.View
-import android.support.v4.app.SupportActivity
 import android.support.v4.app.SupportActivity.ExtraData
-import android.support.v4.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
 
 
 
@@ -30,21 +29,37 @@ class MainActivity : AppCompatActivity() {
     lateinit var diceImage: ImageView
     lateinit var diceImage2: ImageView
 
+    lateinit var imgNumOne: ImageView
+    lateinit var imgNumTwo: ImageView
+    lateinit var imgNumThree: ImageView
+    lateinit var imgNumFour: ImageView
+    lateinit var imgNumFive: ImageView
+    lateinit var imgNumSix: ImageView
+    lateinit var imgNumSeven: ImageView
+    lateinit var imgNumEight: ImageView
+    lateinit var imgNumNine: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        this.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        this.window.decorView.systemUiVisibility = (SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or SYSTEM_UI_FLAG_FULLSCREEN
+                or SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
 
         createListeners()
 
         diceImage = findViewById(R.id.dice_image)
         diceImage2 = findViewById(R.id.dice_image2)
+//        val highlight = resources.getDrawable(R.drawable.highlight)
+//        val imgNumOne: ImageView = findViewById(R.id.numberOne)
+//
+//        imgNumOne.setBackground(highlight)
+
+
 
     }
 
@@ -54,35 +69,41 @@ class MainActivity : AppCompatActivity() {
             rollDice()
         }
 
-        val imgNumOne: ImageView = findViewById(R.id.numberOne)
+        imgNumOne = findViewById(R.id.numberOne)
         imgNumOne.setOnClickListener{ Toast.makeText(this, "ONE", Toast.LENGTH_SHORT).show() }
 
-        val imgNumTwo: ImageView = findViewById(R.id.numberTwo)
+        imgNumTwo = findViewById(R.id.numberTwo)
         imgNumTwo.setOnClickListener{ Toast.makeText(this, "TWO", Toast.LENGTH_SHORT).show() }
 
-        val imgNumThree: ImageView = findViewById(R.id.numberThree)
+        imgNumThree = findViewById(R.id.numberThree)
         imgNumThree.setOnClickListener{ Toast.makeText(this, "THREE", Toast.LENGTH_SHORT).show() }
 
-        val imgNumFour: ImageView = findViewById(R.id.numberFour)
+        imgNumFour = findViewById(R.id.numberFour)
         imgNumFour.setOnClickListener{ Toast.makeText(this, "FOUR", Toast.LENGTH_SHORT).show() }
 
-        val imgNumFive: ImageView = findViewById(R.id.numberFive)
+        imgNumFive = findViewById(R.id.numberFive)
         imgNumFive.setOnClickListener{ Toast.makeText(this, "Five", Toast.LENGTH_SHORT).show() }
 
-        val imgNumSix: ImageView = findViewById(R.id.numberSix)
+        imgNumSix = findViewById(R.id.numberSix)
         imgNumSix.setOnClickListener{ Toast.makeText(this, "Six", Toast.LENGTH_SHORT).show() }
 
-        val imgNumSeven: ImageView = findViewById(R.id.numberSeven)
+        imgNumSeven = findViewById(R.id.numberSeven)
         imgNumSeven.setOnClickListener{ Toast.makeText(this, "Seven", Toast.LENGTH_SHORT).show() }
 
-        val imgNumEight: ImageView = findViewById(R.id.numberEight)
+        imgNumEight = findViewById(R.id.numberEight)
         imgNumEight.setOnClickListener{ Toast.makeText(this, "Eight", Toast.LENGTH_SHORT).show() }
 
-        val imgNumNine: ImageView = findViewById(R.id.numberNine)
+        imgNumNine = findViewById(R.id.numberNine)
         imgNumNine.setOnClickListener{ Toast.makeText(this, "Nine", Toast.LENGTH_SHORT).show() }
     }
 
     private fun rollDice() {
+        var imgList : MutableList<ImageView> = mutableListOf(imgNumOne, imgNumTwo, imgNumThree,
+                imgNumFour, imgNumFive, imgNumSix, imgNumSeven, imgNumEight, imgNumNine)
+        for(i in 0 until imgList.count()){
+            imgList[i].setBackground(null)
+        }
+
         val randomInt1 = Random().nextInt(6) + 1
         val randomInt2 = Random().nextInt(6) + 1
 
@@ -104,65 +125,126 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-
         diceImage.setImageResource(drawableResource)
         diceImage2.setImageResource(drawableResource2)
 
-        val total = randomInt1 + randomInt2
-        possibleChoices(total)
+        var total = randomInt1 + randomInt2
+
+        val out = IntArray(total)
+
+//        var combinationList : MutableList<Int> = mutableListOf()
+
+        // print all combination of numbers from 1 to n having sum n
+        recur(1, total, out, 0)
+        Log.d("myTag", combinationList.toString())
+
+        loopOfDuplicates()
+
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+//        rollButton.isEnabled = false
+        checkForLargeNum()
+        Log.d("Done", combinationList.toString())
+        val txtInfo: TextView = findViewById(R.id.info_txt)
+        txtInfo.text = combinationList.toString()
+
+        checkWhichToHighlight()
+
+        combinationList.clear()
     }
 
-    private fun possibleChoices(total: Int){
-        //FIX
-        val choicesList = mutableListOf<Int>()
-        choicesList.add(total)
-        val finalList = mutableListOf<Int>()
-        val newList = allChoices(choicesList, 0, total, total, finalList)
-        var strCombinations = ""
-        for (i in 0 until newList.count()){
-            strCombinations = newList.joinToString()
+    fun checkWhichToHighlight(){
+        val highlight = resources.getDrawable(R.drawable.highlight)
+        var imgList : MutableList<ImageView> = mutableListOf(imgNumOne, imgNumTwo, imgNumThree,
+                imgNumFour, imgNumFive, imgNumSix, imgNumSeven, imgNumEight, imgNumNine)
+        for(i in 0 until combinationList[0].count()){
+//            val drawableRes = when (combinationList[0][1]) {
+//                1 -> R.drawable.number_1
+//                2 -> R.drawable.number_2
+//                3 -> R.drawable.number_3
+//                4 -> R.drawable.number_4
+//                5 -> R.drawable.number_5
+//                6 -> R.drawable.number_6
+//                7 -> R.drawable.number_7
+//                8 -> R.drawable.number_8
+//                else -> R.drawable.number_9
+//            }
+            imgList[combinationList[0][i] - 1].setBackground(highlight)
+//            drawableRes.setBackground(highlight)
         }
-        Toast.makeText(this, strCombinations, Toast.LENGTH_SHORT)
-
     }
 
-    fun allChoices(arr: MutableList<Int>, index: Int, num: Int, reducedNum: Int, finalList: MutableList<Int>): MutableList<Int> {
-        if (reducedNum < 0){
-            return finalList
-        }
-        // If combination is
-        // found, print it
-        if (reducedNum == 0)
-        {
-            for (i in 0 until index){
-                finalList.add(arr[i])
+    fun checkForLargeNum(){
+        for (i in 0 until combinationList.count()){
+            if(i < combinationList.count()) {
+                Log.d("Test", combinationList.count().toString())
+                if (combinationList[i].contains(10) || combinationList[i].contains(11)
+                        || combinationList[i].contains(12)) {
+                    combinationList.removeAt(i)
+                    checkForLargeNum()
+                }
             }
-            return finalList
         }
 
-        // Find the previous number
-        // stored in arr[] It helps
-        // in maintaining increasing order
-        val prev: Int
-        if(index == 0){
-            prev = 1
-        }else{
-            prev = arr[index - 1]
-        }
-//        val prev = (index == 0) ? 1 : arr[index - 1];
+    }
 
-        // note loop starts from previous
-        // number i.e. at array location
-        // index - 1
-        for (k in prev until num)
-        {
-            // next element of array is k
-            arr[index] = k
-
-            // call recursively with
-            // reduced number
-            allChoices(arr, index + 1, num, reducedNum - k, finalList)
+    private fun loopOfDuplicates(){
+        for (i in 0 until combinationList.count() - 1){
+            if(i < combinationList.count()){
+                val checkBool = checkForDuplicates(combinationList[i])
+                if(!checkBool){
+                    combinationList.removeAt(i)
+                    loopOfDuplicates()
+                }
+            }
         }
-        return finalList
+    }
+
+    private var combinationList : MutableList<MutableList<Int>> = mutableListOf()
+
+    fun checkForDuplicates(listToCheck : MutableList<Int>) : Boolean{
+
+        for (i in 0 until listToCheck.count() - 1) {
+            if (listToCheck[i] === listToCheck[i + 1]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun recur(i: Int, n: Int, out: IntArray, index: Int) {
+        // if sum becomes n, print the combination
+        var newCombinationList : MutableList<Int> = mutableListOf()
+        if (n == 0) {
+            //trying to pass combinationList as a mutable list that contains all possible combinations but parameters in kotlin are CONSTANTS for some ungodly reason.
+            printCombination(out, index)
+        }
+
+        var j: Int
+
+        // start from previous element in the combination till n
+        j = i
+        while (j <= n) {
+            // place current element at current index
+            out[index] = j
+
+            // recur with reduced sum
+            recur(j, n - j, out, index + 1)
+            j++
+        }
+//        return newCombinationList
+    }
+
+    fun printCombination(out: IntArray, n: Int){
+        var cobList : MutableList<Int> = mutableListOf()
+        for (i in 0 until n) {
+//            System.out.printf("%d ", out[i])
+            cobList.add(out[i])
+        }
+        combinationList.add(cobList)
+//        combinationList.add(singleCombinationList)
+        Log.d("myTag", cobList.toString())
+        Log.d("myTag", "-----------------------")
+
     }
 }
